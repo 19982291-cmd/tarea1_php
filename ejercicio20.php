bca116ab40522fd7fae087333af73d092a86c25d
19982291-cmd <19982291@clases.edu.sv>
2026-05-18 20:42:12 -0600

    Ejercicio 20
    Cree un formulario que permita ingresar un número y envíe el dato a otro archivo PHP donde se realice la suma de los números desde 1 hasta el número ingresado.

---
+++ ejercicio20.php
@@ -0,0 +1,10 @@
+<?php
+$num = $_POST['num'];
+$suma = 0;
+for ($i = 1; $i <= $num; $i++) {
+    $suma = $i + $suma;
+}
+echo "El resultado de la suma de 1 hasta " . $num . " da como resultado " . $suma;
+echo "<br><br>";
+echo '<a href="index.php">Volver</a>';
+?>
