0c68225e36034409e0ff8c1d3a46c3e7f49e5d6d
19982291-cmd <19982291@clases.edu.sv>
2026-05-18 20:02:38 -0600

    Ejercicio 4
    Cree un formulario que solicite dos números y envíe los datos a otro archivo PHP donde se calcule y muestre la multiplicación.

---
+++ ejercicio4.php
@@ -0,0 +1,8 @@
+<?php
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$mult = $num1 * $num2;
+echo "La multiplicación de sus dos numeros es de: " . $mult;
+echo "<br><br>";
+echo '<a href="index.php">Volver</a>';
+?> 
