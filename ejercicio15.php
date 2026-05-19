4a3e5ef7859d98a99e5efb1e8e76143f1f7c7cc0
19982291-cmd <19982291@clases.edu.sv>
2026-05-18 20:22:04 -0600

    Ejercicio 15
    Elabore un formulario que solicite una temperatura en grados Celsius y envíe la información a otro archivo PHP donde se realice la conversión a grados Fahrenheit.

---
+++ ejercicio15.php
@@ -0,0 +1,7 @@
+<?php
+$celsius = $_POST['celsius'];
+$fahr = ($celsius * 9/5) + 32;
+echo "Los grados Celsius que ingreso convertidos a grados Fahrenheit son " . $fahr . "°F";
+echo "<br><br>";
+echo '<a href="index.php">Volver</a>';
+?>
