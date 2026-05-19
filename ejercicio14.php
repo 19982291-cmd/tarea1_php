976564ac5aed965327d4aacac99376af48095225
19982291-cmd <19982291@clases.edu.sv>
2026-05-18 20:21:24 -0600

    Ejercicio 14
    Cree un formulario que permita ingresar una cantidad en metros y envíe el dato a otro archivo PHP donde se convierta y muestre la cantidad en centímetros.

---
+++ ejercicio14.php
@@ -0,0 +1,7 @@
+<?php
+$metros = $_POST['metros'];
+$cent = $metros * 100;
+echo "La cantidad de metros que ingreso convertidas a centimetros es de " . $cent . "cm";
+echo "<br><br>";
+echo '<a href="index.php">Volver</a>';
+?>
