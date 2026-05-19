e5a4afbd5c222e3f252a879698765d0f22f4ecd3
19982291-cmd <19982291@clases.edu.sv>
2026-05-18 20:26:07 -0600

    Ejercicio 16
    Diseñe un formulario que solicite el nombre del empleado, las horas trabajadas y el pago por hora. Los datos deben enviarse a otro archivo PHP donde se calcule y muestre el salario total.

---
+++ ejercicio16.php
@@ -0,0 +1,9 @@
+<?php
+$nombre = $_POST['nombre'];
+$horas = $_POST['horas'];
+$sueldo = $_POST['sueldo'];
+$pago = $horas * $sueldo;
+echo "<center>Estimado " . $nombre . ", usted trabajo " . $horas . " horas con un pago de $" . $sueldo . " dolares por hora y por eso su salario total es de $" . $pago . " dolares.</center>";
+echo "<br><br>";
+echo '<a href="index.php">Volver</a>';
+?>
