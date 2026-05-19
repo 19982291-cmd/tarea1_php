6ea11d144c64ffb1166b3883d4fbcf78ea66df45
19982291-cmd <19982291@clases.edu.sv>
2026-05-18 20:05:30 -0600

    Ejercicio 7
    Diseñe un formulario que solicite dos números y envíe los datos a otro archivo PHP donde se determine y muestre cuál número es mayor.

---
+++ ejercicio7.php
@@ -0,0 +1,11 @@
+<?php
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+if ($num1 > $num2) {
+    echo $num1 . " es mayor que " . $num2;
+} else {
+    echo $num2 . " es mayor que " . $num1;
+}
+echo "<br><br>";
+echo '<a href="index.php">Volver</a>';
+?>
